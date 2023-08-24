e4546b5174475e9f331f87b4ae0f2d35d337d60d
Valentina <valentinaelisa2022@gmail.com>
2023-08-24 11:28:58 -0400

    Calculo con if con menú

--- calc.js
+++ calc.js
@@ -11,11 +11,23 @@ var n1,n2, suma;
 
 seguir = prompt("Bienvenido a la calculadora, quiere seguir? s/n: ");
 if (seguir =="s"){
-nombre = prompt ("ingrese nombre: ");
-n1 = parseInt(prompt("Ingrese número 1: "));
-n2 = parseFloat(prompt("Ingrese número 2: "));
-suma = n1+n2;
-console.log(suma);}
-else
-console.log("Chau")
+    opcion= prompt("1.- suma, 2.- resta 3.- Multiplicacion 4.- Division, que quiere hacer? ");
+    if (opcion ==1){
+        console.log("esto es suma ")
+        n1 = parseInt(prompt("Ingrese número 1: "));
+        n2 = parseFloat(prompt("Ingrese número 2: "));
+        suma = n1+n2;
+        console.log(suma);}
+}
+else if (opcion ==2)
+{
+    console.log (" Esto es resta")
+}
+else if (opcion ==3)
+{
+    console.log (" Esto es Multiplicación ")
+}
+else {
+    console.log(" Esto es division ")
+}
 
